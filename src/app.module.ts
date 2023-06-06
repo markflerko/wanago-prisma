275eb151550661da29b4fb9aea652443132c3932
Mark Flerko <markflerko@gmail.com>
2023-06-06 17:51:49 +0300

    feat(): auth, address

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationModule } from 'src/authentication/authentication.module';
+import { PostsModule } from 'src/post/posts.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsModule } from 'src/post/posts.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    AuthenticationModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UserNotFoundException } from 'src/user/user-not-found.exception';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getByEmail(email: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      throw new UserNotFoundException();
+    }
+
+    return user;
+  }
+
+  async getById(id: number) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!user) {
+      throw new UserNotFoundException();
+    }
+
+    return user;
+  }
+
+  async create(user: CreateUserDto) {
+    const address = user.address;
+    return this.prismaService.user.create({
+      data: {
+        ...user,
+        address: {
+          create: address,
+        },
+      },
+    });
+  }
+}
