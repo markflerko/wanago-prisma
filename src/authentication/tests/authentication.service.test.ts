1d77759fc8fffc8d632626a4a60e8242c068166e
markflerko <markflerko@gmail.com>
2023-08-14 00:05:36 +0300

    add few tests

--- src/authentication/tests/authentication.service.test.ts
+++ src/authentication/tests/authentication.service.test.ts
@@ -1,22 +1,34 @@
+import { BadRequestException, HttpException, HttpStatus } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { Test } from '@nestjs/testing';
-import { PrismaModule } from '../../prisma/prisma.module';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { UserNotFoundException } from 'src/user/user-not-found.exception';
+import { UserService } from 'src/user/user.service';
 import { AuthenticationService } from '../../authentication/authentication.service';
-import { UsersModule } from '../../user/user.module';
 
 describe('The AuthenticationService', () => {
   let authenticationService: AuthenticationService;
+  let password: string;
+  let getByEmailMock: jest.Mock;
   beforeEach(async () => {
+    getByEmailMock = jest.fn();
     const module = await Test.createTestingModule({
-      providers: [AuthenticationService],
+      providers: [
+        AuthenticationService,
+        {
+          provide: UserService,
+          useValue: {
+            getByEmail: getByEmailMock,
+          },
+        },
+      ],
       imports: [
-        UsersModule,
         ConfigModule.forRoot(),
         JwtModule.register({
           secretOrPrivateKey: 'Secret key',
         }),
-        PrismaModule,
       ],
     }).compile();
 
@@ -26,6 +38,46 @@ describe('The AuthenticationService', () => {
     it('should return a correct string', () => {
       const result = authenticationService.getCookieForLogOut();
       expect(result).toBe('Authentication=; HttpOnly; Path=/; Max-Age=0');
+    });
+  });
+  describe('when the getAuthenticatedUser method is called', () => {
+    describe('and a valid email and password are provided', () => {
+      let userData: User;
+      beforeEach(async () => {
+        password = 'strongPassword123';
+        const hashedPassword = await bcrypt.hash(password, 10);
+        userData = {
+          id: 1,
+          email: 'john@smith.com',
+          name: 'John',
+          password: hashedPassword,
+          addressId: null,
+        };
+        getByEmailMock.mockResolvedValue(userData); // 👈
+      });
+      it('should return the new user', async () => {
+        const result = await authenticationService.getAuthenticatedUser(
+          userData.email,
+          password,
+        );
+        expect(result).toBe(userData);
+      });
+    });
+    describe('and an invalid email is provided', () => {
+      beforeEach(() => {
+        getByEmailMock.mockRejectedValue(new UserNotFoundException()); // 👈
+      });
+      it('should throw a HttpException with status code 400', async () => {
+        try {
+          await authenticationService.getAuthenticatedUser(
+            'john@smith.com',
+            password,
+          );
+        } catch (error) {
+          expect(error).toBeInstanceOf(HttpException);
+          expect(error.getStatus()).toBe(HttpStatus.BAD_REQUEST);
+        }
+      });
     });
   });
 });
