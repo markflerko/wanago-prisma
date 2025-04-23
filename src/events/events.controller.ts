2f2c41c986d870afc2247b219b88e7171ac23c6c
markflerko <markflerko@gmail.com>
2025-04-23 19:13:15 +0300

    finish

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { TransformPlainToInstance } from 'class-transformer';
+import { CreateEventDto } from 'src/events/dto/events.dto';
+import { FindEventsParamsDto } from 'src/events/dto/find-events-params.dto';
+import { EventsService } from 'src/events/events.service';
+import { EventResponseDto } from 'src/events/response/event-response.dto';
 
 @Controller('events')
-export class EventsController {}
+export class EventsController {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @Get()
+  @TransformPlainToInstance(EventResponseDto)
+  getAll(@Query() { date }: FindEventsParamsDto) {
+    if (date) {
+      return this.eventsService.search(date);
+    }
+    return this.eventsService.getAll();
+  }
+
+  @Post()
+  @TransformPlainToInstance(EventResponseDto)
+  async create(@Body() dto: CreateEventDto) {
+    return this.eventsService.create(dto);
+  }
+}
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [EventsController],
-  providers: [EventsService]
+  providers: [EventsService],
 })
 export class EventsModule {}
